0c10b1c6ebc4833a27c4b6879eef9b74126c9b24
M946i <484616632@qq.com>
2024-04-10 00:08:26 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h3>模板语法</h3>
+  <p>{{msg}}</p>
+  <p>{{hello.split(" ").reverse().join(" ")}}</p>
+  <p v-html="rawHtml"></p>
+  <div v-bind:class="act">测试</div>
+</template>
+
+<script>
+ export default{
+  data() {
+    return {
+      msg:"神奇的语法",
+      hello:"Hello World",
+      rawHtml:"<a href='https://itbaizhan.com'>百战程序员</a>",
+      act:"active"
+    }
+  },
+ }
+</script>
