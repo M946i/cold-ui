d684ffe8baae7551a763c7472da4b3315235df00
M946i <484616632@qq.com>
2024-04-15 18:34:18 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
-  <h3>模板语法</h3>
-  <p>{{msg}}</p>
-  <p>{{hello.split(" ").reverse().join(" ")}}</p>
-  <p v-html="rawHtml"></p>
-  <div v-bind:class="act">测试</div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
- export default{
-  data() {
-    return {
-      msg:"神奇的语法",
-      hello:"Hello World",
-      rawHtml:"<a href='https://itbaizhan.com'>百战程序员</a>",
-      act:"active"
-    }
-  },
- }
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
